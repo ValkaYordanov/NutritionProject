131fe68ae2b02a5bb137f06e9a5520081bc45f72
ValentinYordanov <valka_yordanov@abv.bg>
2021-05-07 10:14:20 +0300

    Commit Meal, Intake and Ingridient class

---
+++ NutritionApp/Models/Ingredient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutritionApp.Models
+{
+    public class Ingredient
+    {
+        public int IngredientId { get; set; }
+        public int ProductId { get; }
+        public decimal Qunatity { get; set; }
+
+    }
+}
