c49f660091643bd68b4aaf210a5e46070361cd6c
AndreasFifty <70367879+AndreasFifty@users.noreply.github.com>
2021-05-07 09:11:54 +0200

    Product and producttag Models

---
+++ NutritionApp/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutritionApp.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int ProductNutrition { get; set; }
+        public string Brand { get; set; }
+        public string ProductName { get; set; }
+        public int GTIN { get; set; }
+    }
+}
