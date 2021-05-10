57c70be7194b167eb69a56d84ffe9f337b4d62b1
Elena Porras <m.elenaporras@hotmail.com>
2021-05-10 13:26:01 +0200

    Register

--- NutritionApp/Controllers/AccountController.cs
+++ NutritionApp/Controllers/AccountController.cs
@@ -3,6 +3,12 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace NutritionApp.Controllers
@@ -10,13 +16,18 @@ namespace NutritionApp.Controllers
     [Authorize]
     public class AccountController : Controller
     {
+
+        private readonly ILogger<AccountController> _logger;
+
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         public AccountController(UserManager<AppUser> userMgr,
-        SignInManager<AppUser> signinMgr)
+        SignInManager<AppUser> signinMgr,
+        ILogger<AccountController> logger)
         {
             _userManager = userMgr;
             _signInManager = signinMgr;
+            _logger = logger;
         }
 
         [AllowAnonymous]
@@ -48,6 +59,38 @@ namespace NutritionApp.Controllers
             return View(details);
         }
 
+        // GET: /Account/Create
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Create(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        // POST: /Account/Create
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateModel model, string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new AppUser { UserName = model.Email, Email = model.Email };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    _logger.LogInformation(3, "User created a new account with password.");
+                    return Redirect(returnUrl ?? "/");
+                }
+                AddErrors(result);
+            }
+
+            return View(model);
+        }
+
         [Authorize]
         public async Task<IActionResult> Logout()
         {
@@ -59,6 +102,14 @@ namespace NutritionApp.Controllers
         public IActionResult AccessDenied()
         {
             return View();
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
     }
 }
