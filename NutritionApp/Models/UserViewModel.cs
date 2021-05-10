312b2caf645378a7dc313823b866c7c28bb93d85
Elena Porras <m.elenaporras@hotmail.com>
2021-05-10 11:43:29 +0200

    Login page done

--- NutritionApp/Models/UserViewModel.cs
+++ NutritionApp/Models/UserViewModel.cs
@@ -7,26 +7,23 @@ using Microsoft.AspNetCore.Identity;
 
 namespace NutritionApp.Models
 {
-    public class UserViewModel
+    public class CreateModel
     {
-        public class CreateModel
-        {
-            [Required]
-            public string Name { get; set; }
-            [Required]
-            public string Email { get; set; }
-            [Required]
-            public string Password { get; set; }
-        }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
 
-        public class LoginModel
-        {
-            [Required]
-            [UIHint("email")]
-            public string Email { get; set; }
-            [Required]
-            [UIHint("password")]
-            public string Password { get; set; }
-        }
+    public class LoginModel
+    {
+        [Required]
+        [UIHint("email")]
+        public string Email { get; set; }
+        [Required]
+        [UIHint("password")]
+        public string Password { get; set; }
     }
 }
