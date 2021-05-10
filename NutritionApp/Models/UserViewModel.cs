a252902fae8e65f4e3f9c66766c46bb3f52d7ed5
Elena Porras <m.elenaporras@hotmail.com>
2021-05-10 09:36:18 +0200

    UserViewModel

---
+++ NutritionApp/Models/UserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace NutritionApp.Models
+{
+    public class UserViewModel
+    {
+        public class CreateModel
+        {
+            [Required]
+            public string Name { get; set; }
+            [Required]
+            public string Email { get; set; }
+            [Required]
+            public string Password { get; set; }
+        }
+
+        public class LoginModel
+        {
+            [Required]
+            [UIHint("email")]
+            public string Email { get; set; }
+            [Required]
+            [UIHint("password")]
+            public string Password { get; set; }
+        }
+    }
+}
