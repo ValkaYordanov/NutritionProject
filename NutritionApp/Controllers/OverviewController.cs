1ee7b4d1ca30349a74c58b0a4075990c76b28c2e
AndreasFifty <70367879+AndreasFifty@users.noreply.github.com>
2021-05-11 12:48:32 +0200

    .

---
+++ NutritionApp/Controllers/OverviewController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutritionApp.Controllers
+{
+    public class OverviewController : Controller
+    {
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+        public ActionResult Index(string id)
+        {
+            if (id != null)
+            {
+                // Lookup topic from id
+                ViewBag.Msg = "You're logged in: Your ID is " + id;
+                ViewBag.Id = id;
+            } else
+            {
+                ViewBag.Msg = "You're NOT logged in";
+            }
+
+            
+            return View();
+        }
+    }
+}
