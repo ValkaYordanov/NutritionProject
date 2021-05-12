9a370290c2c0320d1aebe01bb5d6d6be3300468f
AndreasFifty <70367879+AndreasFifty@users.noreply.github.com>
2021-05-12 14:06:56 +0200

    ...

---
+++ NutritionApp/Controllers/OverviewController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NutritionApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NutritionApp.Controllers
+{
+    public class OverviewController : Controller
+    {
+
+        private readonly UserManager<AppUser> _userManager;
+        public OverviewController(UserManager<AppUser> userMgr)
+        {
+            _userManager = userMgr;
+        }
+
+        private Task<AppUser> CurrentUser => _userManager.FindByNameAsync(HttpContext.User.Identity.Name);
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
