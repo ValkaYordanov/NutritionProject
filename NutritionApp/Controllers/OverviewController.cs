aee6619febbe845d38e9544a77dae9f7233cca5d
ValentinYordanov <valka_yordanov@abv.bg>
2021-05-13 11:38:52 +0300

    delete warnings

--- NutritionApp/Controllers/OverviewController.cs
+++ NutritionApp/Controllers/OverviewController.cs
@@ -4,18 +4,12 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.Rendering;
+
 using Microsoft.EntityFrameworkCore;
 using NutritionApp.Data;
-using NutritionApp.Models;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
-using System.ComponentModel.DataAnnotations;
-using Microsoft.Extensions.Logging;
+
 using System.Security.Claims;
 
 
